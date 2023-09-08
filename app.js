14761d957fded31219c66eedebb71bafb85a2f7b
Jacob <Jakelausier7878@gmail.com>
2023-09-08 10:19:52 -0400

    folder structure

---
+++ app.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Header from './components/Header';
+import Navigation from './components/Navigation';
+import AboutMe from './pages/AboutMe';
+import Portfolio from './pages/Portfolio';
+import Contact from './pages/Contact';
+import Resume from './pages/Resume';
+
+function App() {
+  return (
+    <Router>
+      <Header />
+      <Navigation />
+      <Switch>
+        <Route path="/" exact component={AboutMe} />
+        <Route path="/portfolio" component={Portfolio} />
+        <Route path="/contact" component={Contact} />
+        <Route path="/resume" component={Resume} />
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
